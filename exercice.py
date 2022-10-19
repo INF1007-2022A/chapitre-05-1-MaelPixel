06f3ae7a07b36b2aad422dc6cef88cef3c1e199d
MaelPixel <113027326+MaelPixel@users.noreply.github.com>
2022-10-19 14:43:16 -0400

    dernier exo complet

--- exercice.py
+++ exercice.py
@@ -6,68 +6,68 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    if number<0:
-        absnb=number/-1
+    if number < 0:
+        absnb = number / -1
     else:
-        absnb=number
+        absnb = number
     return absnb
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-    liste=[]
+    liste = []
     for c in prefixes:
-        liste.append((c)+suffixe)
+        liste.append((c) + suffixe)
 
-    return [liste]
+    return liste
 
 
 def prime_integer_summation() -> int:
-    number=0
+    number = 0
     for nb in range(101):
-            number+=nb
+        if nb==1:
+            pass
+        else:
+            number += nb
     return number
 
 
 def factorial(number: int) -> int:
-    nb=1
-    while number>1:
-        nb*=(number*(number-1))
-        number-=2
+    nb = 1
+    while number > 1:
+        nb *= (number)
+        number -= 1
     return nb
 
 
 def use_continue() -> None:
-    k=[]
-    for c in range(1,11):
-        if c==5:
-            pass
+    k = []
+    for c in range(1, 11):
+        if c == 5:
+            continue
         else:
             k.append(c)
     print(k)
 
 
-
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-   liste=[]
-   taille=0
+    liste = []
+    taille = 0
     for c in groups:
-        taille=len(c)
-        if taille> 10 or taille <= 3:
-           liste = liste
+        taille = len(c)
+        if taille > 10 or taille < 4:
+            continue
         for x in c:
-            if x==25:
+            if x == 25:
                 liste.append(c)
-                break
-            if x<18:
-                break
-            if x>70 or x==50:
                 continue
-                if x>70 or x==50:
-                    break
-                else:
-                    liste.append(c)
+            if x < 18:
+                continue
+        if 50 in c and max(c)>70:
+            continue
 
+        liste.append(c)
+    return liste
 
 def main() -> None:
     number = -4.325
@@ -79,14 +79,14 @@ def main() -> None:
 
     number = 10
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
-    
+
     print(f"L'affichage de la boucle est:")
     use_continue()
 
     groups = [
         [15, 28, 65, 70, 72], [18, 24, 22, 50, 70], [25, 2],
-              [20, 22, 23, 24, 18, 75, 51, 49, 100, 18, 20, 20], [70, 50, 26, 28], [75, 50, 18, 25],
-              [13, 25, 80, 15], [20, 30, 40, 50, 60], [75, 50, 100, 28]
+        [20, 22, 23, 24, 18, 75, 51, 49, 100, 18, 20, 20], [70, 50, 26, 28], [75, 50, 18, 25],
+        [13, 25, 80, 15], [20, 30, 40, 50, 60], [75, 50, 100, 28]
     ]
     print(f"Les différents groupes sont: {groups}")
     print(f"L'acceptance des groupes est: {verify_ages(groups)}")
